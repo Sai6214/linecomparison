b0d2eb47e8b68238feb8b3c91cdeb314decc3b08
Saikumar <muthyalasaikumar9@gmail.com>
2022-05-02 16:24:51 +0530

    first commit

--- linecomparison/Program.cs
+++ linecomparison/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 7, x2 = 1, y1 = 8, y2 = 8;
+            int Line1 = (int)Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine(Line1);
+            Console.ReadLine();
+        }
+    }
+}
